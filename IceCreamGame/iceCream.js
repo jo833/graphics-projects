9788bc99b2a546379693130de8c40c1c89dc7cea
Julia Oghigian <77372272+jo833@users.noreply.github.com>
2024-02-24 19:54:17 -0600

    Update iceCream.js

--- IceCreamGame/iceCream.js
+++ IceCreamGame/iceCream.js
@@ -1,14 +1,3 @@
-//////////////// FILE HEADER //////////////////////////
-//
-// Title:    Programming Assignment 1
-// Course:   CS 559 Fall 2022
-//
-// Author:   Julia Oghigian
-// Email:    oghigian@wisc.edu
-// Lecturer: Eftychios Sifakis
-//
-//////////////////////////////////////////////////////
-
 function setup() {
   "use strict";
   var canvas = document.getElementById("myCanvas");
